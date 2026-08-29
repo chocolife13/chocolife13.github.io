29bfccf995581608a7484f9736bf8f2e8563cbff
chocolife13 <carte.evolbie@gmail.com>
2026-08-29 14:41:29 +0200

    sound and year

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const bg = new Audio('assets/duvet.m4a')
 function music(){
-    bg.pause()
+    bg.pause();
     bg.volume = 0.1;
     bg.play();
 }
 music(); 
+
+const hoverSound = new Audio('/assets/tik.wav');
+hoverSound.volume = 1;
+
+function hover(){
+  hoverSound.pause()
+  hoverSound.currentTime = 0;
+  hoverSound.play()
+}
+
+document.querySelectorAll('.hey').forEach(element => {
+  element.addEventListener('mouseenter', () => {
+    hoverSound.currentTime = 0;
+    hoverSound.play().catch(() => {});
+  });
+});
+
+
+const birthDate = '2009-10-24';
+const diffMs = Date.now() - new Date(birthDate).getTime();
+const ageDate = new Date(diffMs);
+const age = Math.abs(ageDate.getUTCFullYear() - 1970);
+
+document.getElementById('year').textContent = age;
